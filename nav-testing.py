9ddb2d067e311708106efd74390f512d103a0a1f
Daniel <danielshrives@gmail.com>
2019-10-04 13:03:27 -0400

    removed kalman filter

--- nav-testing.py
+++ nav-testing.py
@@ -76,78 +76,6 @@ a = datetime.datetime.now()
 # Setup Pubber
 pubber = Publisher(client_id="nav-pubber")
 
-# ------------- Kalman Filter for Gyro ------------------
-def kalmanFilterY ( accAngle, gyroRate, DT):
-		y=0.0
-		S=0.0
-
-		global KFangleY
-		global Q_angle
-		global Q_gyro
-		global y_bias
-		global YP_00
-		global YP_01
-		global YP_10
-		global YP_11
-
-	KFangleY = KFangleY + DT * (gyroRate - y_bias)
-
-	YP_00 = YP_00 + ( - DT * (YP_10 + YP_01) + Q_angle * DT )
-	YP_01 = YP_01 + ( - DT * YP_11 )
-	YP_10 = YP_10 + ( - DT * YP_11 )
-	YP_11 = YP_11 + ( + Q_gyro * DT )
-
-	y = accAngle - KFangleY
-	S = YP_00 + R_angle
-	K_0 = YP_00 / S
-	K_1 = YP_10 / S
-	
-	KFangleY = KFangleY + ( K_0 * y )
-	y_bias = y_bias + ( K_1 * y )
-	
-	YP_00 = YP_00 - ( K_0 * YP_00 )
-	YP_01 = YP_01 - ( K_0 * YP_01 )
-	YP_10 = YP_10 - ( K_1 * YP_00 )
-	YP_11 = YP_11 - ( K_1 * YP_01 )
-	
-	return KFangleY
-
-def kalmanFilterX ( accAngle, gyroRate, DT):
-	x=0.0
-	S=0.0
-
-	global KFangleX
-	global Q_angle
-	global Q_gyro
-	global x_bias
-	global XP_00
-	global XP_01
-	global XP_10
-	global XP_11
-
-
-	KFangleX = KFangleX + DT * (gyroRate - x_bias)
-
-	XP_00 = XP_00 + ( - DT * (XP_10 + XP_01) + Q_angle * DT )
-	XP_01 = XP_01 + ( - DT * XP_11 )
-	XP_10 = XP_10 + ( - DT * XP_11 )
-	XP_11 = XP_11 + ( + Q_gyro * DT )
-
-	x = accAngle - KFangleX
-	S = XP_00 + R_angle
-	K_0 = XP_00 / S
-	K_1 = XP_10 / S
-	
-	KFangleX = KFangleX + ( K_0 * x )
-	x_bias = x_bias + ( K_1 * x )
-	
-	XP_00 = XP_00 - ( K_0 * XP_00 )
-	XP_01 = XP_01 - ( K_0 * XP_01 )
-	XP_10 = XP_10 - ( K_1 * XP_00 )
-	XP_11 = XP_11 - ( K_1 * XP_01 )
-	
-	return KFangleX
-
 def publish_gps_status():
 	message = {
 		'time' :  agps_thread.data_stream.time,
@@ -242,14 +170,6 @@ def publish_internal_compass_status():
 		AccYangle -= 270.0
 	else:
 		AccYangle += 90.0
-
-	#Complementary filter used to combine the accelerometer and gyro values.
-	CFangleX=AA*(CFangleX+rate_gyr_x*LP) +(1 - AA) * AccXangle
-	CFangleY=AA*(CFangleY+rate_gyr_y*LP) +(1 - AA) * AccYangle
-
-	#Kalman filter used to combine the accelerometer and gyro values.
-	kalmanY = kalmanFilterY(AccYangle, rate_gyr_y,LP)
-	kalmanX = kalmanFilterX(AccXangle, rate_gyr_x,LP)
 
 	if IMU_UPSIDE_DOWN:
 		MAGy = -MAGy      #If IMU is upside down, this is needed to get correct heading.
