36eb3fd87c56cb323e48642ed46e1bcbcc6041be
danielshrives <danielshrives@gmail.com>
2019-10-23 23:13:10 -0400

    print

--- nav-testing.py
+++ nav-testing.py
@@ -282,20 +282,16 @@ def publish_vector():
 
 	for x in gps_targets:
 
-		distance = 5
+		distance = 5 # initialize non-zero
 
 		while(distance > TARGET_RADIUS):
-
-			# current_lat = float(agps_thread.data_stream.lat)
-			# current_lon = float(agps_thread.data_stream.lon)
-			# current_pos = (current_lat,current_lon)
-			# print(current_pos)
 
 			target_lat = float(x['latitude'])
 			target_lon = float(x['longitude'])
 			target_pos = (target_lat,target_lon)
-			#print(target_pos)
-			
+
+			print(current_pos)
+			print(target_pos)
 			distance = haversine(current_pos,target_pos,unit=Unit.NAUTICAL_MILES)
 			print(distance)
 			
