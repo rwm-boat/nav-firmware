c3b63c8baba839a3c304f0bb98c9631e8b26681c
danielshrives <danielshrives@gmail.com>
2019-10-25 03:19:12 -0400

    a

--- nav-testing.py
+++ nav-testing.py
@@ -283,7 +283,7 @@ def publish_vector():
 			print(message)
 			app_json = json.dumps(message)
 			pubber.publish("/status/vector",app_json)
-			#time.sleep(1)
+			time.sleep(1)
 	
 def on_led_command(client, userdata, message):
 	obj = json.loads(message.payload.decode('utf-8'))
@@ -317,8 +317,8 @@ subber = Subscriber(client_id="led_actuator", broker_ip="192.168.1.170", default
 thread = Thread(target=subber.listen)
 thread.start()
 
-# vector_thread = Thread(target=publish_vector)
-# vector_thread.start()
+vector_thread = Thread(target=publish_vector)
+vector_thread.start()
 
 counter = 0
 
