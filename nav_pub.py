17086fbb22d60365f4ecaed93407c261066ef2e6
danielshrives <danielshrives@gmail.com>
2019-10-25 09:36:53 -0400

    formatting

--- nav_pub.py
+++ nav_pub.py
@@ -221,6 +221,20 @@ def publish_compas_status():
 
 def publish_vector():
 
+	# -------- MAGNITUDE CONSTANTS ---------
+	# 5 - full chat
+	# 4 - comfortable planning
+	# 3 - min-plane-speed
+	# 2 - max efficency displacement
+	# 1 - low speed trolling
+	# 0 - stop
+	max_speed = 0.5 # ~ 100 meters
+	plane = 0.5 
+	min_plane = 0.5
+	max_efficency = 0.15 # ~ 25m
+	troll = 0.0025 # ~ 5m
+	# ---------------------------------------
+
 	global distance
 
 	TARGET_RADIUS = 0.002
@@ -236,61 +250,42 @@ def publish_vector():
 			"longitude" : -71.816851
 		}
 	]
-
+	#itterate through list of gps targets
 	for x in gps_targets:
 
 		distance = 5 # initialize non-zero
-		print("WAYPOINT HIT")
-
+		
+	# while you have not yet hit the target create vectors
 		while(distance > TARGET_RADIUS):
 
 			try:
 				target_lat = float(x['latitude'])
 				target_lon = float(x['longitude'])
 				target_pos = (target_lat,target_lon)
+				cur_lat = current_lat
+				cur_lon = current_lon
 				distance = haversine(current_pos,target_pos,unit=Unit.NAUTICAL_MILES)
 			except Exception:
 				pass
 
-
-				#print("non-valid gps values")
-			
-			# -------- MAGNITUDE CONSTANTS ---------
-			# 5 - full chat
-			# 4 - comfortable planning
-			# 3 - min-plane-speed
-			# 2 - max efficency displacement
-			# 1 - low speed trolling
-			# 0 - stop
-
-			max_speed = 0.5 # ~ 100 meters
-			plane = 0.5 
-			min_plane = 0.5
-			max_efficency = 0.15 # ~ 25m
-			troll = 0.0025 # ~ 5m
-			# ---------------------------------------
-
 			# calculate vector between two points 
 			target_point = wgs84.GeoPoint(latitude=target_lat, longitude=target_lon, z=0, degrees = True)
-			current_point = wgs84.GeoPoint(latitude=current_lat, longitude=current_lon, z=0, degrees = True)
+			current_point = wgs84.GeoPoint(latitude=cur_lat, longitude=cur_lon, z=0, degrees = True)
 			p_AB_N = current_point.delta_to(target_point)
 			azimuth = p_AB_N.azimuth_deg[0]
 			angle = azimuth
-
+			if(angle < 0):
+				angle += 360
+			
 			# calculate magnitude from distance
-			try:
-				distance = haversine(current_pos,target_pos, unit=Unit.NAUTICAL_MILES)
-				
-			except Exception:
-				pass
-				#("non-valid gps")
+			distance = haversine(current_pos,target_pos, unit=Unit.NAUTICAL_MILES)
 			if(distance > plane): magnitude = 5
 			elif(distance > min_plane): magnitude = 4
 			elif(distance > max_efficency): magnitude = 3
 			else: magnitude = 1
 			print("distance: " + str(distance))
-			if(angle < 0):
-				angle += 360
+			
+
 			# post message with new data
 			message = {
 				'heading' : angle,
