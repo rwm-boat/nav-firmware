e2d460c91db7e5db9b00c7ab3c04c4cf19681b52
danielshrives <danielshrives@gmail.com>
2019-10-25 13:18:06 -0400

    a

--- vector_pub.py
+++ vector_pub.py
@@ -45,52 +45,50 @@ def publish_vector():
 	# 		"longitude" : -71.816851
 	# 	}
 	# ]
-
-	with open('gps_waypoints.txt', "r") as json_file:
-		gps_targets = json.load(json_file)
-
+	
 	#itterate through list of gps targets
-	for line in gps_targets.readlines():
-		x = json.loads(line)
-		distance = 5 # initialize non-zero
+	with open('gps_waypoints.txt', "r") as json_file:
+		for line in json_file.readlines():
+			x = json.loads(line)
+			distance = 5 # initialize non-zero
 
-	# while you have not yet hit the target create vectors
-		while(distance > TARGET_RADIUS):
+		# while you have not yet hit the target create vectors
+			while(distance > TARGET_RADIUS):
 
-			try:
-				target_lat = float(x['latitude'])
-				target_lon = float(x['longitude'])
-				target_pos = (target_lat,target_lon)
-				cur_lat = float(gps_pub.current_lat)
-				cur_lon = float(gps_pub.current_lon)
-				current_position = (cur_lat,cur_lon)
-				distance = haversine(current_position,target_pos,unit=Unit.NAUTICAL_MILES)
-			except Exception:
-				print("vector pub not receiving valid gps")
-			if(distance < 10): # get rid of outliers
-				# calculate vector between two points 
-				target_point = wgs84.GeoPoint(latitude=target_lat, longitude=target_lon, z=0, degrees = True)
-				current_point = wgs84.GeoPoint(latitude=cur_lat, longitude=cur_lon, z=0, degrees = True)
-				p_AB_N = current_point.delta_to(target_point)
-				azimuth = p_AB_N.azimuth_deg[0]
-				angle = azimuth
-				if(angle < 0):
-					angle += 360
-				
-				# calculate magnitude from distance
-				if(distance > 10): magnitude = 1 # remove outliers
-				elif(distance > plane): magnitude = 5
-				elif(distance > min_plane): magnitude = 4
-				elif(distance > max_efficency): magnitude = 3
-				else: magnitude = 1
-				print("distance: " + str(distance))
-		
-				# post message with new data
-				message = {
-					'heading' : angle,
-					'magnitude' : magnitude,
-				}
-				print(message)
-				app_json = json.dumps(message)
-				pubber.publish("/status/vector",app_json)
-			time.sleep(1)
+				try:
+					target_lat = float(x['latitude'])
+					target_lon = float(x['longitude'])
+					target_pos = (target_lat,target_lon)
+					cur_lat = float(gps_pub.current_lat)
+					cur_lon = float(gps_pub.current_lon)
+					current_position = (cur_lat,cur_lon)
+					distance = haversine(current_position,target_pos,unit=Unit.NAUTICAL_MILES)
+				except Exception:
+					print("vector pub not receiving valid gps")
+				if(distance < 10): # get rid of outliers
+					# calculate vector between two points 
+					target_point = wgs84.GeoPoint(latitude=target_lat, longitude=target_lon, z=0, degrees = True)
+					current_point = wgs84.GeoPoint(latitude=cur_lat, longitude=cur_lon, z=0, degrees = True)
+					p_AB_N = current_point.delta_to(target_point)
+					azimuth = p_AB_N.azimuth_deg[0]
+					angle = azimuth
+					if(angle < 0):
+						angle += 360
+					
+					# calculate magnitude from distance
+					if(distance > 10): magnitude = 1 # remove outliers
+					elif(distance > plane): magnitude = 5
+					elif(distance > min_plane): magnitude = 4
+					elif(distance > max_efficency): magnitude = 3
+					else: magnitude = 1
+					print("distance: " + str(distance))
+			
+					# post message with new data
+					message = {
+						'heading' : angle,
+						'magnitude' : magnitude,
+					}
+					print(message)
+					app_json = json.dumps(message)
+					pubber.publish("/status/vector",app_json)
+					time.sleep(1)
